dd49a574e48f279efb9fea8742adca9fdba5b850
SwanHub <jdprince555@gmail.com>
2019-08-06 16:06:59 -0600

    Done.

--- app.rb
+++ app.rb
@@ -2,11 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
+
+
+	get '/' do # the route here is '/'
+		erb :index # :index refers to index.erb view
 	end
 
-	get "/info" do
-      erb :info
+	get "/info" do # the route here is '/info'
+      erb :info # :info refers to info.erb view
   end
 end
